37ae011c6ad082f5bfb7b3da2d9e49d328b25c2b
qwazar030501 <faraondk34@mail.ru>
2025-12-12 09:42:20 +0300

    крмит3

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -16,27 +16,4 @@ void main() {
 
     free(s2);
 }
-char* CreateNewStringP(char* s1) {
-    int count_lower = 0;
-    int len = 0;
-    char* ptr = s1;
-    while (*ptr != '\0') {
-        if (islower(*ptr) && isalpha(*ptr)) {
-            count_lower++;
-        }
-        len++;
-        ptr++;
-    }
-    int new_len = len + count_lower;
-    char* new_str = (char*)malloc((new_len + 1) * sizeof(char));
-    char* dest = new_str;
-    for (int i = 0; i < count_lower; i++) {
-        *dest++ = '&';
-    }
-    ptr = s1;
-    while (*ptr != '\0') {
-        *dest++ = *ptr++;
-    }
-    *dest = '\0';
-    return new_str;
-}
+
